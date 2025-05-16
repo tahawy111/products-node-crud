b1fa23c723e4edbba8e7fc4267be572eeaf0391b
amer <amertahawy111@gmail.com>
2025-05-16 20:05:12 +0300

    Video 05,06,07 Enhance product management functionality by updating routes and views for dynamic product handling, including detailed product viewing, editing, and deletion features.

--- routes/products.js
+++ routes/products.js
@@ -5,22 +5,31 @@ const router = Router();
 
 // Define your routes here
 router.get("/", async (req, res) => {
-
   const products = await Product.find();
   console.log(products);
-  
 
   res.render("index", { products });
 });
 router.get("/add-product", async (req, res) => {
   res.render("add-product");
 });
-router.get("/view-product", async (req, res) => {
-  res.render("view-product");
+router.get("/view-product/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+
+  res.render("view-product", { product });
 });
-router.get("/edit-product", async (req, res) => {
-  res.render("edit-product");
+router.get("/edit-product/:id", async (req, res) => {
+  const { id } = req.params;
+  const product = await Product.findById(id);
+  res.render("edit-product", { product });
 });
+router.get("/delete-product/:id", async (req, res) => {
+  const { id } = req.params;
+  await Product.findByIdAndDelete(id);
+  res.redirect("/");
+});
+
 
 // POST REQUESTS
 
@@ -49,6 +58,39 @@ router.post("/add-product", async (req, res) => {
     res.redirect("/");
   } catch (error) {
     console.error("Error saving product:", error);
+    return res.status(500).send("Internal Server Error");
+  }
+});
+
+router.post("/edit-product/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  
+  const product = await Product.findById(id);
+  if (!product) {
+    return res.status(404).send("Product not found");
+  }
+
+  const {
+    productname,
+    productprice,
+    productdescription,
+    productimageurl,
+    badges,
+  } = req.body;
+
+  try {
+    product.productname = productname;
+    product.productprice = productprice;
+    product.productdescription = productdescription;
+    product.productimageurl = productimageurl;
+    product.badges = badges;
+
+    await product.save();
+
+    res.redirect("/view-product/" + id);
+  } catch (error) {
+    console.error("Error updating product:", error);
     return res.status(500).send("Internal Server Error");
   }
 });
